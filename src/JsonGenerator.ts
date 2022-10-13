a40691476f779da5e057697680b440933d056ba4
MasatoMakino <makino.masato.g@gmail.com>
2022-10-13 23:00:06 +0900

    add : summary.json出力

--- src/JsonGenerator.ts
+++ src/JsonGenerator.ts
@@ -21,16 +21,41 @@ export class JsonGenerator {
     for (let item of items) {
       item.bodyHTML = await this.convertToHTML(item.bodyContent);
       item.preview = await this.convertToPreview(item.bodyContent);
-
-      const filePath = path.resolve(
-        options.contentsDir,
-        options.jsonDir,
-        `${format(new Date(item.created_at), "yyyy-MM-dd-HHmmss_")}${
-          item.id
-        }.json`
+      await fs.writeFile(
+        this.getFilePath(item, options),
+        JSON.stringify(item, null, 2)
       );
-      fs.writeFile(filePath, JSON.stringify(item, null, 2));
     }
+
+    const summary = { flatMap: {}, sourceFileArray: [] };
+    for (let item of items) {
+      const filePath = path.relative(
+        process.cwd(),
+        this.getFilePath(item, options)
+      );
+      summary.flatMap[filePath] = {
+        title: item.title,
+        created_at: item.created_at,
+        categories: item.categories,
+        preview: item.preview,
+      };
+      summary.sourceFileArray.push(filePath);
+    }
+
+    await fs.writeFile(
+      path.resolve(options.contentsDir, options.jsonDir, `summary.json`),
+      JSON.stringify(summary, null, 2)
+    );
+  }
+
+  private static getFilePath(item, options?: Options): string {
+    return path.resolve(
+      options.contentsDir,
+      options.jsonDir,
+      `${format(new Date(item.created_at), "yyyy-MM-dd-HHmmss_")}${
+        item.id
+      }.json`
+    );
   }
 
   static async convertToHTML(body: string) {
