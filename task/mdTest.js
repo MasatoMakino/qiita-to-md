a545a3bc0b6bdb03b35fd2ec5300f164f437f62a
MasatoMakino <makino.masato.g@gmail.com>
2023-08-31 17:50:27 +0900

    fix : duplication of test

--- task/mdTest.js
+++ task/mdTest.js
@@ -2,20 +2,8 @@ import { JsonGenerator } from "../bin/JsonGenerator.js";
 
 //language=markdown
 const result = await JsonGenerator.convertToHTML(`
-## テスト 
-
-
-ただのパラグラフ
-
-:::note
-ノート
-:::
 
 :::note{.warn}
-ノート
-:::
-
-:::note warn
 ノート
 :::
 
